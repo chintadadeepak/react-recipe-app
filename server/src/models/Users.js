eabc92978d323d9b17b4bf3e58590f7498368c30
Deepak <chintadadeepak31@gmail.com>
2023-08-23 17:38:30 +0530

    wrote code for register and login

---
+++ server/src/models/Users.js
@@ -0,0 +1,16 @@
+// defining how should our users collection look like..
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = mongoose.model("users", UserSchema);
