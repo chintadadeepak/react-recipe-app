90e4cd5f9a5498975a809cc8575ddd2461808a73
Deepak <chintadadeepak31@gmail.com>
2023-08-25 15:49:43 +0530

    components of the project

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/navbar.css";
+import { useCookies } from "react-cookie";
+
+function Navbar() {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+  const logOut = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userId");
+    navigate("/auth");
+  };
+  return (
+    <div className="navbar">
+      <div className="nav-links">
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/create-recipe">
+          Create Recipe
+        </Link>
+        <Link className="nav-link" to="/save-recipe">
+          Saved Recipes
+        </Link>
+        {!cookies.access_token ? (
+          <Link className="nav-link" to="/auth">
+            Register/Login
+          </Link>
+        ) : (
+          <button onClick={logOut}>Logout</button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
